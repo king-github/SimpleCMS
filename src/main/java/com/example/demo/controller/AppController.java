5cebf48a28fe382748f10df99610d6d38090ad62
Krzystof K <king@autograf.pl>
2018-11-19 19:50:29 +0100

    Refactor AppController

--- src/main/java/com/example/demo/controller/AppController.java
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -13,11 +13,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.format.DateTimeFormatter;
@@ -49,13 +49,8 @@ public class AppController {
                         @PageableDefault(size = ARTICLES_PER_PAGE) Pageable pageable
                         ){
 
-
         logger.info("Home page");
 
-        String[] items = {"First", "Second", "Third"};
-
-        model.addAttribute("items", items);
-        addDefaultAttributeToModel(model);
         model.addAttribute("articles", articleService.getAllArticles( pageable ));
         model.addAttribute("title", "Home Page");
 
@@ -66,9 +61,8 @@ public class AppController {
     public String showArticle (@PathVariable Long id, Model model){
 
         logger.info("Get article id={}", id);
-        Article article = articleService.findArticle(id);
 
-        addDefaultAttributeToModel(model);
+        Article article = articleService.findArticle(id);
         model.addAttribute("article", article);
         model.addAttribute("title", article.getTitle());
 
@@ -83,10 +77,6 @@ public class AppController {
 
         logger.info("Home page");
 
-        String[] items = {"First", "Second", "Third"};
-
-        model.addAttribute("items", items);
-        addDefaultAttributeToModel(model);
         model.addAttribute("articles", articleService.findArticleByAuthor(id, pageable ));
         model.addAttribute("title", "Home Page");
 
@@ -100,12 +90,7 @@ public class AppController {
 
         logger.info("Tag Page - id: "+id);
 
-        String[] items = {"First", "Second", "Third"};
-
         Tag tag = tagService.findTagById(id);
-
-        model.addAttribute("items", items);
-        addDefaultAttributeToModel(model);
         model.addAttribute("articles", articleService.findArticleByTag(tag.getId(), pageable ));
         model.addAttribute("title", "Tag: "+tag.getName());
 
@@ -120,18 +105,14 @@ public class AppController {
 
         logger.info("Tag Page - name: "+name);
 
-        String[] items = {"First", "Second", "Third"};
-
         Tag tag = tagService.findTagByName(name);
-
-        model.addAttribute("items", items);
-        addDefaultAttributeToModel(model);
         model.addAttribute("articles", articleService.findArticleByTag(tag.getId(), pageable ));
         model.addAttribute("title", "Tag: "+tag.getName());
 
         return "default/index";
     }
 
+    @ModelAttribute
     private void addDefaultAttributeToModel(Model model) {
         model.addAttribute("tags", tagService.getAllTagsWithQuantity());
         model.addAttribute("authors", authorService.getAllAuthorsWithQuantity());
